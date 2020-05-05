dc5ac787a0f9b45e5778a0d4b8530c46cb59e791
Maylisdr <maylis.desrochettes@gmail.com>
2020-05-05 11:10:28 +0200

    Ajout Lac, Perdu, congrats

---
+++ app/src/main/java/com/example/applimontcalmes/enigmemedecin.java
@@ -0,0 +1,45 @@
+package com.example.applimontcalmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class enigmemedecin extends AppCompatActivity {
+
+    private Button Suivant;
+    private Button precedent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enigmemedecin);
+        Suivant = (Button) findViewById(R.id.Suivant);
+        Suivant.setOnClickListener(suivantListener);
+        precedent = (Button) findViewById(R.id.precedent);
+        precedent.setOnClickListener(revenirListener);
+
+    }
+
+    private View.OnClickListener revenirListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+
+    };
+
+
+    private void ActiviteSuivante (){
+        Intent intent = new Intent (this,jevaispasmourir.class );
+        startActivity(intent);
+    }
+
+    private View.OnClickListener suivantListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ActiviteSuivante ();
+        }
+    };
+}
